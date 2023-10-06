6be1e9051f5976a74624b395672a8a7b2cf7a931
Preocts <preocts@preocts.com>
2023-10-06 18:05:27 -0400

    Add dollar amount filter

--- src/htmx_fastapi/_filters.py
+++ src/htmx_fastapi/_filters.py
@@ -6,7 +6,7 @@ import datetime
 from fastapi.templating import Jinja2Templates
 
 
-def _readable_timestamp(timestamp: int, tzoffset: int = 0) -> str:
+def _to_date(timestamp: int, tzoffset: int = 0) -> str:
     """Return a human-readable timestamp."""
     _tzoffset = datetime.timedelta(hours=tzoffset)
     dt = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
@@ -14,6 +14,12 @@ def _readable_timestamp(timestamp: int, tzoffset: int = 0) -> str:
     return dt.strftime("%Y-%m-%d")
 
 
+def _to_dollars(amount: int) -> str:
+    """Return a human-readable amount."""
+    return f"{amount / 100:.2f}"
+
+
 def apply_filters(template: Jinja2Templates) -> None:
     """Apply all defined filters to the template."""
-    template.env.filters["readable_timestamp"] = _readable_timestamp
+    template.env.filters["to_date"] = _to_date
+    template.env.filters["to_dollars"] = _to_dollars
