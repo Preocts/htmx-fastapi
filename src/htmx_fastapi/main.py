fe31cca42e69d03b3bba7047d8f2a09d23f7b5c0
Preocts <preocts@preocts.com>
2023-10-05 22:40:40 -0400

    Create edit and single row fetch

--- src/htmx_fastapi/main.py
+++ src/htmx_fastapi/main.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import datetime
 import sqlite3
+from typing import Annotated
 
 import fastapi
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from . import _filters
+from .transaction import Transaction
 from .transactionstore import TransactionStore
 
 # Setup TransactionStore
@@ -50,3 +52,101 @@ def transactions(
     }
 
     return template.TemplateResponse("partial/transactions.html", context)
+
+
+@app.get("/transaction/{transaction_id}")
+def transaction(
+    request: fastapi.Request,
+    transaction_id: int,
+) -> fastapi.Response:
+    """
+    Return partial HTML for a single transaction.
+    """
+    context = {
+        "request": request,
+        "transaction": transaction_store.get_by_id(transaction_id),
+    }
+
+    return template.TemplateResponse("partial/transaction_row.html", context)
+
+
+@app.get("/transaction/{transaction_id}/edit")
+def edit_transaction(request: fastapi.Request, transaction_id: int) -> fastapi.Response:
+    """
+    Return partial HTML for editing a single transaction.
+    """
+    row = transaction_store.get_by_id(transaction_id)
+
+    context = {
+        "request": request,
+        "transaction": row,
+    }
+
+    return template.TemplateResponse("partial/transaction_form.html", context)
+
+
+@app.put("/transaction/{transaction_id}")
+def update_transaction(
+    request: fastapi.Request,
+    transaction_id: int,
+    date_time: Annotated[str, fastapi.Form()],
+    description: Annotated[str, fastapi.Form()],
+    amount: Annotated[int, fastapi.Form()],
+) -> fastapi.Response:
+    """
+    Update a single transaction.
+    """
+    try:
+        timestamp = int(datetime.datetime.strptime(date_time, "%Y-%m-%d").timestamp())
+    except ValueError:
+        timestamp = int(datetime.datetime.now(tz=datetime.timezone.utc).timestamp())
+
+    transaction = Transaction(transaction_id, amount, description, timestamp)
+
+    transaction_store.update(transaction)
+
+    context = {
+        "request": request,
+        "transaction": transaction,
+    }
+
+    return template.TemplateResponse("partial/transaction_row.html", context)
+
+
+@app.delete("/transaction/{transaction_id}")
+def delete_transaction(
+    request: fastapi.Request, transaction_id: int
+) -> fastapi.Response:
+    """
+    Delete a single transaction.
+    """
+    transaction_store.delete(transaction_id)
+
+    return fastapi.Response(status_code=200)
+
+
+# @app.post("/transaction")
+# def create_transaction(
+#     request: fastapi.Request,
+#     date_time: str,
+#     description: str,
+#     amount: int,
+# ) -> fastapi.Response:
+#     """
+#     Create a transaction.
+#     """
+#     try:
+#         timestamp = int(datetime.datetime.fromisoformat(date_time).timestamp())
+#     except ValueError:
+#         timestamp = int(datetime.datetime.now(tz=datetime.timezone.utc).timestamp())
+
+#     transaction = Transaction(0, amount, description, timestamp)
+
+#     transaction_store.add(transaction)
+
+#     context = {
+#         "request": request,
+#         "transaction": transaction,
+#     }
+
+#     return template.TemplateResponse("partial/transaction_row.html", context)
