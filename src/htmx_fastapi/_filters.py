4898b6f20ee1a13553723632927d0ba5ebd4487a
Preocts <preocts@preocts.com>
2023-10-09 18:21:43 -0400

    Enhance filter with default and offset days

--- src/htmx_fastapi/_filters.py
+++ src/htmx_fastapi/_filters.py
@@ -4,12 +4,26 @@ from __future__ import annotations
 import datetime
 
 from fastapi.templating import Jinja2Templates
+from jinja2.runtime import Undefined
 
 
-def _to_date(timestamp: int, tzoffset: int = 0) -> str:
-    """Return a human-readable timestamp."""
+def _to_date(timestamp: int | Undefined, dayoffset: int = 0, tzoffset: int = 0) -> str:
+    """
+    Return a human-readable date in the format YYYY-MM-DD.
+
+    If timestamp is not provided, the current time is used.
+
+    Args:
+        timestamp: A unix timestamp.
+        dayoffset: The number of days to offset the timestamp by.
+        tzoffset: The number of hours to offset the timestamp by.
+    """
+    if isinstance(timestamp, Undefined):
+        now = datetime.datetime.now(tz=datetime.timezone.utc)
+        timestamp = int((now - datetime.timedelta(days=dayoffset)).timestamp())
+
     _tzoffset = datetime.timedelta(hours=tzoffset)
-    dt = datetime.datetime.fromtimestamp(timestamp, tz=datetime.timezone.utc)
+    dt = datetime.datetime.fromtimestamp(int(timestamp), tz=datetime.timezone.utc)
     dt = dt + _tzoffset
     return dt.strftime("%Y-%m-%d")
 
